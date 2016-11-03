e9dcd554a0dd0cca6aeb9d8e1f74a92a261a26d1
ritikasardana <ritika.sardana12@gmail.com>
2016-11-03 15:57:02 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+function math() {
+
+var m = 16;
+var n = 17;
+
+if (m == true){
+	alert("16 is less than 17");
+  console.log("n is true");
+}
+
+else if (m == s) {
+	confirm("16 = 17")
+  console.log("m is false");
+}
+
+else {
+	console.log("16 is greater than 17");
+}
